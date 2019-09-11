cc7eaf76a011b10b6682ea995d7630453002d56d
Patrick Cox <coxpatrick74@gmail.com>
2019-09-11 13:19:00 -0400

    Setup project from template

---
+++ src/LuceneQueryParser.cpp
@@ -0,0 +1,5 @@
+#include <lqueryparser/LuceneQueryParser.hpp>
+
+namespace mytemplate
+{
+}  // namespace mytemplate
---
+++ tests/src/test_main.cpp
@@ -0,0 +1,3 @@
+#include <lqueryparser/LuceneQueryParser.hpp>
+
+int main() { return 0; }
